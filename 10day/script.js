35a15881097cd759ac74002eef09ae7e08431d6e
adarsh-mishra07 <viratadarshmishra07837@gmail.com>
2025-10-20 15:21:06 +0530

    Day 10: Joke Generator Project

---
+++ 10day/script.js
@@ -0,0 +1,15 @@
+const jokeBtn = document.getElementById("jokeBtn");
+const jokeDiv = document.getElementById("joke");
+
+async function getJoke() {
+    joke.innerText = "Loading joke..";
+    try {
+        const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+        const data = await response.json();
+        jokeDiv.innerHTML = `${data.setup}🤡 ${data.punchline}`;
+
+    } catch (error) {
+        jokeDiv.innerHTML = "Oops! Couldn't load a jpoke";
+    }
+}
+jokeBtn.addEventListener("click", getJoke);
